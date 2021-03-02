c19b1e324b3f6ccd4656f0512ff14ec64292cd3b
SOVEREIGN <isimemeni@gmail.com>
2021-03-01 22:58:00 -0500

    added images and js file

---
+++ dragonball/src/questions.js
@@ -0,0 +1,68 @@
+//Questions
+var questions = [
+    {
+        question: "Whos death pushed Goku to go Super Saiyan for the first time?",
+        answer: ["Gohan", "Piccolo", "Tien", "Krillin"],
+        correct: "Krillin",
+        image: './assets/images/krillin.gif',
+    },
+    {
+        question:"Which fusion only lasts for 30 minutes?",
+        answer: ["Potara-Fusion", "Fusion-Dance", "Namekian-Fusion", "Majin-Fusion"],
+        correct: "Fusion-Dance",
+        image: './assets/images/fusion.gif',
+    },
+    {
+        question:"Which universe won the Tournament of Power?",
+        answer: ["Universe-9", "Universe-7", "Universe-5", "Universe-8"],
+        correct: "Universe-7",
+        image: './assets/images/universe7.png',
+    },
+    {
+        question:"Who is the most reoccuring Dragon Ball movie villian?",
+        answer: ["Broly", "Cooler", "Bojack", "Janemba"],
+        correct: "Broly",
+        image: './assets/images/broly.gif',
+    },
+    {
+        question:"Which character did Buu not absorb?",
+        answer: ["Gohan", "Gotenks", "Piccolo", "Tien"],
+        correct: "Tien",
+        image: './assets/images/tien.jpg',
+    },
+    {   
+        question:"Which new level did Goku reach during the Tournament of Power?",
+        answer: ["Super Saiyan Blue", "Ultra-Instinct", "Super Saiyan Rose", "Super Saiyan 4"],
+        correct: "Ultra-Instinct",
+        image: './assets/images/ultrainstinct.gif',
+    },
+    {   question:"How many androids did Cell need to absorb to become Perfect Cell?",
+        answer: ["4", "2", "6", "1"],
+        correct: "2",
+        image: './assets/images/androids.gif',
+    },
+    {
+        question:"Which character defeated Cell?",
+        answer: ["Gohan", "Goku", "Vegeta", "Trunks"],
+        correct: "Gohan",
+        image: './assets/images/gohan.gif',
+    },
+    {
+        question:"Which character did Babidi take control of during the Buu saga?",
+        answer: ["Vegeta", "Goku", "Piccolo", "Goten"],
+        correct: "Vegeta",
+        image: './assets/images/majinvegeta.gif',
+    },
+    {
+        question:"What was the name of the organization that created all of the androids in Dragon Ball?",
+        answer: ["Capsule-Corporation", "Red-Ribbon-Army", "Ginyu-Force", "Cooler's Armored Squadron"],
+        correct: "Red-Ribbon-Army",
+        image: './assets/images/redribbon.jpg',
+    },
+    {    
+        question:"Which character was able to befriend Majin Buu and turn him good?",
+        answer: ["Gotenks", "Hercule", "Bibidi", "Yamcha"],
+        correct: "Hercule",
+        image: './assets/images/hercule.gif',
+    },
+    ]   
